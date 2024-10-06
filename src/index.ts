60ab9273a19c9fa8bd0d254b17acc03021a969d4
nichoth <nichoth@nichoth.com>
2024-10-06 16:02:33 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,107 @@
-import { createDebug } from '@bicycle-codes/debug'
-const debug = createDebug()
+import { x25519 } from '@noble/curves/ed25519'
+import { xchacha20poly1305 } from '@noble/ciphers/chacha'
+import { sha256, sha512 } from '@noble/hashes/sha2'
+import { hkdf } from '@noble/hashes/hkdf'
+import { fromString } from 'uint8arrays'
+import { webcrypto } from '@bicycle-codes/one-webcrypto'
+// import { createDebug } from '@bicycle-codes/debug'
+// const debug = createDebug()
 
-export function example ():void {
-    debug('hello')
+export interface Message {
+    pub:string;
+    content:string;
+}
+
+export interface Keys {
+    privateKey:Uint8Array;
+    publicKey:Uint8Array;
+}
+
+/**
+ * Generate some random bytes.
+ */
+function createRandom (length?:number) {
+    const seed = new Uint8Array(length ?? 32)
+    const random = webcrypto.getRandomValues(seed)
+    return random
+}
+
+/**
+ * Create a new x25519 keypair.
+ *
+ * @param seed Private key material
+ * @returns {keys} A new keypair
+ */
+export function create (seed:Uint8Array = createRandom()):Keys {
+    const sk = sha256(seed)
+    const pk = x25519.scalarMultBase(sk)
+
+    return { privateKey: sk, publicKey: pk }
+}
+
+export interface Message {
+    keys:{  // <-- base64pad encoded keys
+        public:string,
+        prevPublic:string
+    };
+    content:string;
+}
+
+/**
+ * Create a new message.
+ *
+ * @param text The text to encrypt
+ * @param keypair Your ed25519 keypair
+ * @param pub Their ed25519 public key
+ * @param info Info param
+ */
+export function message (
+    text:string|Uint8Array,
+    keypair:{ privateKey:string|Uint8Array, publicKey:string|Uint8Array },
+    pub:string|Uint8Array,
+    info?:string
+):Message {
+    const newSecret = getSecret(keypair, pub, info)
+    const nonce = createRandom()
+    const cipher = xchacha20poly1305(newSecret, nonce)
+    const cipherText = cipher.encrypt(typeof text === 'string' ?
+        fromString(text) :
+        text)
+    const msg = {
+        keys: {
+
+        },
+        body: { text: cipherText }
+    }
+}
+
+/**
+ * Use ECDH to derive a new secret key.
+ *
+ * @param keypair Your keypair
+ * @param pub Their public key
+ * @param info  Info param
+ * @returns {Uint8Array} The new secret key
+ */
+export function getSecret (
+    keypair:{ privateKey:string|Uint8Array, publicKey:string|Uint8Array },
+    pub:string|Uint8Array,
+    info?:string
+):Uint8Array {
+    const { privateKey } = keypair
+
+    const newSecret = hkdf(
+        sha256,
+        x25519.getSharedSecret(privateKey, pub),
+        undefined,
+        info,
+        // (info || (typeof keypair.publicKey === 'string' ?
+        //     keypair.publicKey :
+        //     toString(keypair.publicKey, 'base64pad') +
+        //     (typeof pub === 'string' ? pub : toString(pub, 'base64pad'))
+        // )),
+        32
+    )
+
+    return newSecret
 }
--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,31 @@
+import 'dotenv/config'
+import { equals } from 'uint8arrays'
 import { test } from '@bicycle-codes/tapzero'
-import { example } from '../src/index.js'
+import { type Keys, create, getSecret } from '../src/index.js'
+// import Debug from '@bicycle-codes/debug'
+// const debug = Debug('test')
 
-test('example', async t => {
-    t.ok('ok', 'should be an example')
-    example()
+let alice:Keys
+let bob:Keys
+test('setup', async t => {
+    alice = create()
+    bob = create()
+    t.ok(alice, 'Alice should exist')
+    t.ok(bob, 'Bob should exist')
 })
+
+let key
+test('create a new secret key via ECDH', t => {
+    key = getSecret(alice, bob.publicKey)
+    t.ok(key instanceof Uint8Array, 'should return new bytes')
+})
+
+test('bob derives a key', t => {
+    const bobsKey = getSecret(bob, alice.publicKey)
+    t.ok(bobsKey instanceof Uint8Array, 'should return bytes')
+    t.ok(equals(key, bobsKey), 'should return the same key')
+})
+
+// test('encrypt with the key', t => {
+
+// })
