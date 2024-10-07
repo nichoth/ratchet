10e0d5a7e21c73a1f27cb64e1760d8954e006998
nichoth <nichoth@nichoth.com>
2024-10-06 22:45:43 -0700

    tests pass

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import { x25519 } from '@noble/curves/ed25519'
 import { xchacha20poly1305 } from '@noble/ciphers/chacha'
-import { sha256, sha512 } from '@noble/hashes/sha2'
+import { sha256 } from '@noble/hashes/sha2'
 import { hkdf } from '@noble/hashes/hkdf'
-import { fromString } from 'uint8arrays'
+import { fromString, toString } from 'uint8arrays'
 import { webcrypto } from '@bicycle-codes/one-webcrypto'
+import type { DID } from '@bicycle-codes/crypto-util/types'
 // import { createDebug } from '@bicycle-codes/debug'
 // const debug = createDebug()
-
-export interface Message {
-    pub:string;
-    content:string;
-}
+const NONCE_SIZE = 24
 
 export interface Keys {
     privateKey:Uint8Array;
     publicKey:Uint8Array;
+}
+
+/**
+ * encoded as `base64pad`
+ */
+export interface SerializedKeys {
+    privateKey:string;
+    publicKey:string;
 }
 
 /**
@@ -32,7 +37,7 @@ function createRandom (length?:number) {
  * @param seed Private key material
  * @returns {keys} A new keypair
  */
-export function create (seed:Uint8Array = createRandom()):Keys {
+export function create (seed:Uint8Array = createRandom(NONCE_SIZE)):Keys {
     const sk = sha256(seed)
     const pk = x25519.scalarMultBase(sk)
 
@@ -41,38 +46,138 @@ export function create (seed:Uint8Array = createRandom()):Keys {
 
 export interface Message {
     keys:{  // <-- base64pad encoded keys
-        public:string,
-        prevPublic:string
+        publicKey:string,
     };
-    content:string;
+    author:DID,
+    body: {
+        text:string;
+    }
 }
 
 /**
- * Create a new message.
+ * Create an encrypted message using a new keypair + the other party's most
+ * recent public key.
+ *
+ * Put the public half of the new keypair in the message, so it can
+ * be decrypted with their most recent private key + the public key in
+ * the message.
  *
  * @param text The text to encrypt
  * @param keypair Your ed25519 keypair
  * @param pub Their ed25519 public key
  * @param info Info param
+ * @returns {[Message, { keys:Keys }]} The encrypted message and the new keypair
+ *      used to encrypt it.
  */
 export function message (
     text:string|Uint8Array,
-    keypair:{ privateKey:string|Uint8Array, publicKey:string|Uint8Array },
-    pub:string|Uint8Array,
+    theirPublicKey:string|Uint8Array,
+    author:DID,
+    newKeypair?:{ privateKey:string|Uint8Array, publicKey: string|Uint8Array },
     info?:string
-):Message {
-    const newSecret = getSecret(keypair, pub, info)
-    const nonce = createRandom()
+):[Message, { keys:Keys }] {
+    const keypair = newKeypair || create()
+    const newSecret = getSecret(keypair, theirPublicKey, info)
+    const nonce = createRandom(NONCE_SIZE)
     const cipher = xchacha20poly1305(newSecret, nonce)
     const cipherText = cipher.encrypt(typeof text === 'string' ?
         fromString(text) :
         text)
-    const msg = {
-        keys: {
 
+    //
+    // create this message with our new private key,
+    // + their most recent public key
+    //
+    // put the public half of the new keypair in the message
+    //
+
+    const pub = (typeof keypair.publicKey === 'string' ?
+        keypair.publicKey :
+        toString(keypair.publicKey, 'base64pad'))
+
+    // prepend the nonce to the encrypted content
+    const encryptedContent = new Uint8Array([...nonce, ...cipherText])
+
+    const msg:Message = {
+        keys: {
+            publicKey: pub
         },
-        body: { text: cipherText }
+        author,
+        body: {
+            text: toString(encryptedContent, 'base64pad')
+        }
     }
+
+    return [msg, {
+        keys: {
+            privateKey: (typeof keypair.privateKey === 'string' ?
+                fromString(keypair.privateKey, 'base64pad') :
+                keypair.privateKey),
+            publicKey: (typeof keypair.publicKey === 'string' ?
+                fromString(keypair.publicKey, 'base64pad') :
+                keypair.publicKey
+            )
+        }
+    }]
+}
+
+/**
+ * Encrypt with a symmetric key.
+ * Should be 24 byte nonce -- https://github.com/paulmillr/noble-ciphers?tab=readme-ov-file#encrypt-with-xchacha20-poly1305
+ */
+export function encrypt (
+    key:string|Uint8Array,
+    plainText:string|Uint8Array,
+    nonce?:Uint8Array
+):string {
+    nonce = nonce ?? createRandom(NONCE_SIZE)
+    const cipher = xchacha20poly1305(
+        typeof key === 'string' ? fromString(key, 'base64pad') : key,
+        nonce
+    )
+
+    const encrypted = cipher.encrypt(
+        typeof plainText === 'string' ? fromString(plainText) : plainText,
+    )
+
+    // prepend the nonce to the cipher text
+    return toString(new Uint8Array([...nonce, ...encrypted]), 'base64pad')
+}
+
+export function decryptMsg (msg:Message, keypair:Keys):Message {
+    const secret = getSecret(keypair, msg.keys.publicKey)
+    const cipherText = fromString(msg.body.text, 'base64pad')
+    const nonce = cipherText.slice(0, NONCE_SIZE)
+    const cipherBytes = cipherText.slice(NONCE_SIZE)  // slice 24 -> end
+    const cipher = xchacha20poly1305(secret, nonce)
+    const decrypted = cipher.decrypt(cipherBytes)
+
+    return {
+        ...msg,
+        body: {
+            text: toString(decrypted)
+        }
+    }
+}
+
+/**
+ * Decrypt with a symmetric key.
+ *
+ * @param cipherText The cipher text, including the nonce
+ * @param key The key to decrypt with
+ * @returns {string} The decrypted text
+ */
+export function decrypt (cipherText:string, key:Uint8Array):string {
+    const cipherBytes = fromString(cipherText, 'base64pad')
+    const nonce = cipherBytes.slice(0, NONCE_SIZE)
+    const cipherTextBytes = cipherBytes.slice(NONCE_SIZE)  // slice -- 24 -> end
+    const cipher = xchacha20poly1305(
+        typeof key === 'string' ? fromString(key, 'base64pad') : key,
+        nonce
+    )
+
+    const plainText = cipher.decrypt(cipherTextBytes)
+    return toString(plainText)
 }
 
 /**
@@ -80,26 +185,23 @@ export function message (
  *
  * @param keypair Your keypair
  * @param pub Their public key
- * @param info  Info param
+ * @param info Info param
  * @returns {Uint8Array} The new secret key
  */
 export function getSecret (
     keypair:{ privateKey:string|Uint8Array, publicKey:string|Uint8Array },
-    pub:string|Uint8Array,
+    _pub:string|Uint8Array,
     info?:string
 ):Uint8Array {
     const { privateKey } = keypair
+
+    const pub = typeof _pub === 'string' ? fromString(_pub, 'base64pad') : _pub
 
     const newSecret = hkdf(
         sha256,
         x25519.getSharedSecret(privateKey, pub),
         undefined,
         info,
-        // (info || (typeof keypair.publicKey === 'string' ?
-        //     keypair.publicKey :
-        //     toString(keypair.publicKey, 'base64pad') +
-        //     (typeof pub === 'string' ? pub : toString(pub, 'base64pad'))
-        // )),
         32
     )
 
--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,16 @@
 import 'dotenv/config'
 import { equals } from 'uint8arrays'
 import { test } from '@bicycle-codes/tapzero'
-import { type Keys, create, getSecret } from '../src/index.js'
+import { publicKeyToDid } from '@bicycle-codes/crypto-util/util'
+import {
+    type Keys,
+    create,
+    getSecret,
+    encrypt,
+    decrypt,
+    message,
+    decryptMsg
+} from '../src/index.js'
 // import Debug from '@bicycle-codes/debug'
 // const debug = Debug('test')
 
@@ -14,7 +23,7 @@ test('setup', async t => {
     t.ok(bob, 'Bob should exist')
 })
 
-let key
+let key:Uint8Array
 test('create a new secret key via ECDH', t => {
     key = getSecret(alice, bob.publicKey)
     t.ok(key instanceof Uint8Array, 'should return new bytes')
@@ -26,6 +35,37 @@ test('bob derives a key', t => {
     t.ok(equals(key, bobsKey), 'should return the same key')
 })
 
-// test('encrypt with the key', t => {
+let cipherText:string
+test('encrypt something', t => {
+    const encrypted = cipherText = encrypt(key, 'hello, world of chacha')
+    t.equal(typeof encrypted, 'string', 'should return a string')
+})
 
-// })
+test('decrypt the string', t => {
+    const plainText = decrypt(cipherText, key)
+    t.equal(typeof plainText, 'string', 'should return a string')
+    t.equal(plainText, 'hello, world of chacha', 'should decrypt the message')
+})
+
+let msg
+test('encrypt a message', t => {
+    const [_msg, { keys }] = message(
+        'hello messages',
+        bob.publicKey,
+        publicKeyToDid.ecc(alice.publicKey),
+    )
+    msg = _msg
+
+    console.log('the message...', msg)
+    t.ok(msg, 'returns a message')
+    t.equal(typeof msg.body.text, 'string', 'should return the right shape object')
+    t.equal(msg.author, publicKeyToDid.ecc(alice.publicKey),
+        'should have the right author')
+    t.ok(keys.publicKey instanceof Uint8Array, 'returns keys separately')
+})
+
+test('Bob can decrypt the message that Alice created', t => {
+    const decrypted = decryptMsg(msg, bob)
+    t.ok(decrypted, 'should return something')
+    t.equal(decrypted.body.text, 'hello messages', 'should decrypt the message')
+})
